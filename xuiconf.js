6cf46c4e0facb25d3c769707359afbcc839c200c
onjaysantana <111399274+onjaysantana@users.noreply.github.com>
2024-01-14 10:36:47 -0800

    Updated by CrossUI No-Code App Builder

--- xuiconf.js
+++ xuiconf.js
@@ -1,6 +1,22 @@
 
+
 // [[Page Appearance
-//xui.ini.$PageAppearance = {
-//    "theme":"webflat"
-//};
+xui.ini.$PageAppearance = {
+    "theme" : "webflat"
+};
 // ]]Page Appearance
+
+
+// [[Font Icons CDN
+
+// ]]Font Icons CDN
+
+
+// [[Develop Env Setting
+xui.ini.$DevEnv = {
+    "designViewConf" : {
+        "width" : 800,
+        "height" : 600
+    }
+};
+// ]]Develop Env Setting
